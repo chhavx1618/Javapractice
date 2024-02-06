fda2a060140212f0b5c1542ee4377a0c56938e50
chhavx1618 <152146397+chhavx1618@users.noreply.github.com>
2024-02-06 20:17:29 +0530

    Create simplesort.java

---
+++ simplesort.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class sortarr {
+    public static void main(String[] args) {
+
+        int nums[] = {2,6,8,3,9,4,5};
+
+        System.out.println(Arrays.toString(nums));
+
+        Arrays.sort(nums);
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
